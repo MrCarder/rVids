ad83bde43fb2d30880fdace45706e4223d6ae357
MrCarder <carder.williams@gmail.com>
2014-01-02 17:49:53 -0600

    no chnges, just added beginnings for pocket support

---
+++ RedditVideoTool/pocket_home.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RedditVideoTool
+{
+    public partial class pocket_home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
